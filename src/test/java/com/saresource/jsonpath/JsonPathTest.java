f68bd609eff85d5dfd09a640906403e3b0e5ce61
Robert Umland <robert@umlandweb.com>
2017-08-31 06:36:56 -0600

    Add test showing how to use JSONPath.

---
+++ src/test/java/com/saresource/jsonpath/JsonPathTest.java
@@ -0,0 +1,30 @@
+package com.saresource.jsonpath;
+
+import com.jayway.jsonpath.DocumentContext;
+import com.jayway.jsonpath.JsonPath;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class JsonPathTest {
+    private final InputStream json = this.getClass().getResourceAsStream("production-entities.json");
+    private final DocumentContext context = JsonPath.parse(json);
+
+    @Test
+    public void readSingleFieldTest() throws IOException {
+        assertEquals("16-12V", context.read("$.[0].well_number"));
+    }
+
+    @Test
+    public void readAllFieldInArrayTest() {
+        List<String> read = context.read("$.[*].well_number");
+        assertTrue(read.contains("16-12V"));
+        assertTrue(read.contains("18-01V"));
+        assertTrue(read.contains("14-2"));
+    }
+}
